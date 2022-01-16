ba89f337e20c37b924f39a3eca7a43c3838b839b
Lucas Meira <lucaas.meiraf@gmail.com>
2022-01-16 17:26:56 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Score.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	//PK COMPOSTA SE CRIA UMA CLASSE COM AS CHAVES PRIMÁRIAS E INSTANCIA.
+	private ScorePK id = new ScorePK();
+	
+	private Double value; 
+	
+	public Score() {
+		
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie); 
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+}
